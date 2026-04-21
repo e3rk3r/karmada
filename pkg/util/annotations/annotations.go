23efbdc92114965a9d223966e7833675f7222be9
Erik Eriksen <e3rk3r@users.noreply.github.com>
2026-04-21 11:14:37 +0800

    pkg/util/annotations: fix malformed file header and package doc comment

--- pkg/util/annotations/annotations.go
+++ pkg/util/annotations/annotations.go
@@ -9,19 +9,23 @@ You may obtain a copy of the License at
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND permissions and
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
 limitations under the License.
 */
 
- with
+// Package annotations provides utility functions for working with
 // Kubernetes object annotations in the Karmada control plane.
-package annotations
+ (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
-import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1 GetAnnotationValue retrieves the value// a Kubernetes object's metadata. Returns an empty string if the annotation does not exist or ifunc GetAnnotationValue(meta metav1.ObjectMeta, key string) string {
+// Get a specific
+// a Kubernetes object's metadata. Returns an empty string if the annotation
+// does not exist or if the annotations map is nil.
+func GetAnnotationValue(meta metav1 string {
 	if meta.Annotations == nil {
-		return ""
-	}
+n	}
 	return meta.Annotations[key]
 }
 
@@ -56,6 +60,7 @@ func RemoveAnnotation(meta *metav1.ObjectMeta, key string) {
 // MergeAnnotations merges the src annotations into dst annotations.
 // Existing keys in dst will be overwritten by values from src.
 // Note: this modifies dst in place and also returns it for convenience.
+// If src is nil, dst is returned unchanged.
 func MergeAnnotations(dst, src map[string]string) map[string]string {
 	if dst == nil {
 		dst = make(map[string]string)
